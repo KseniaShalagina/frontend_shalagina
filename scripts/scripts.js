b4f084a0ada898ce02d10c8d9ce4f1df560669cf
KseniaShalagina <Kseniashalagina2803@gmail.com>
2025-04-02 19:49:43 +0000

    js

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+const cards={
+    card_1:{
+        card_name: "Professional Profile",
+        card_text: "We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and getdiscovered by recruiter",
+    },
+    card_2:{
+        card_name: "Best Portfolio",
+        card_text: "We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and getdiscovered by recruiter",
+    },
+    card_3:{
+        card_name: "Powerful Resume",
+        card_text: "We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and getdiscovered by recruiter",
+    },
+}
