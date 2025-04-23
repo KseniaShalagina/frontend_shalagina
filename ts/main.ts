1f0b56f137e794978babcea9bf27f2a5f53dcc02
KseniaShalagina <Kseniashalagina2803@gmail.com>
2025-04-23 16:51:42 +0000

    card 4

---
+++ ts/main.ts
@@ -0,0 +1,13 @@
+interface Card {
+    card_name: string;
+    card_text: string;
+}
+function showCard(Card){
+    //const infCard=document.getElementById("information-cards");
+    String
+    { return
+       
+
+    }
+
+}
